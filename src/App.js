026117e946cc424da55c1374ad56e73697cb4662
Mars Cheung <mars.kwong.cheung@gmail.com>
2022-04-04 23:16:12 -0700

    demonstrate react router dom outlet, route nesting and index

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
 
-import Directory from './components/directory/directory.component';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+import { 
+    Route,
+    Routes,
+} from "react-router-dom";
+
+const Shop = () => {
+	return (
+		<div>
+			I am the shop page.
+		</div>
+	)
+}
 
 const App = () => {
-
-  const categories =[
-    {
-      id:1,
-      title:'Hats',
-	  imageUrl:'https://i.ibb.co/cvpntL1/hats.png'
-    },
-    {
-      id:2,
-      title:'Jackets',
-	  imageUrl:'https://i.ibb.co/px2tCc3/jackets.png'
-    },
-    {
-      id:3,
-      title:'Sneakers',
-	  imageUrl:'https://i.ibb.co/0jqHpnp/sneakers.png'
-    },
-    {
-      id:4,
-      title:'Womens',
-	  imageUrl:'https://i.ibb.co/GCCdy8t/womens.png'
-    },
-    {
-      id:5,
-      title:'Mens',
-	  imageUrl:'https://i.ibb.co/R70vBrQ/mens.png'
-    },
-  ];
   
   return (
-    <Directory categories={categories}/>
+	<Routes>
+		<Route path='/' element={<Navigation/>}>
+			<Route index element={<Home/>}/>
+			<Route path='shop' element={<Shop/>} />
+		</Route>
+	</Routes>
   );
 }
 
