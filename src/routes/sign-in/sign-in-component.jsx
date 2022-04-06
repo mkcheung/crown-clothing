458672b888c69ced84840da789660381bdd707e2
Mars Cheung <mars.kwong.cheung@gmail.com>
2022-04-06 11:21:40 -0700

    demonstrate addition of form object and form item set

--- src/routes/sign-in/sign-in-component.jsx
+++ src/routes/sign-in/sign-in-component.jsx
@@ -1,15 +1,11 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
+import { 
+    signInWithGooglePopup,
+    createUserDocumentFromAuth 
+} from "../../utils/firebase/firebase.utils";
 
-import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
-    useEffect(async ()=>{
-        const response = await getRedirectResult(auth);
-        if(response){
-            const userDocRef = await createUserDocumentFromAuth(response.user);
-        }
-    }, []);
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user)
@@ -19,7 +15,7 @@ const SignIn = () => {
 			<h1>
                 Sign In Page
                 <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-                <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
+                <SignUpForm/>
             </h1>
 		</div>
 	)
